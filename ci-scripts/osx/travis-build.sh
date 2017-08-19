aecba8a72d32712a3829086add6cd9390c7f4fda
Dave Amies <damies13@gmail.com>
2017-08-19 22:04:52 +1000

    make -j 2
    and remove build dir if exists

--- ci-scripts/osx/travis-build.sh
+++ ci-scripts/osx/travis-build.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+
 pushd thirdparty/tiff-4.0.3
 ./configure && make
 popd
-cd toonz && mkdir build && cd build
+
+cd toonz
+if [ -d "build" ]; then
+  rm -rf build
+fi
+mkdir build
+cd build
 cmake ../sources \
       -DQT_PATH=/usr/local/Cellar/qt5/5.6.1-1/lib/ \
       -DTIFF_INCLUDE_DIR=../../thirdparty/tiff-4.0.3/libtiff/ \
       -DSUPERLU_INCLUDE_DIR=../../thirdparty/superlu/SuperLU_4.1/include/
-make
+make -j 2
